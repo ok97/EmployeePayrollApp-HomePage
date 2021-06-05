9ba215abc40ffdf6268ed9d29a840e3794dfb401
ok97 <omkhawshi0@gmail.com>
2021-06-06 01:19:53 +0530

    Added: Creating Home Page UI)

---
+++ js/EmployeePayrollApp.js
@@ -0,0 +1,143 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function () 
+    {
+        if (name.value.length == 0) 
+        {
+            textError.textContent = "";
+            return;
+        }
+        try 
+        {
+            (new EmployeePayroll()).name = name.value;
+            textError.textContent = "";
+        }
+        catch (e) 
+        {
+            textError.textContent = e;
+        }
+    });
+/* UC2:- Ability to set Event Listeners when Document is loaded so as to.
+         - Set Event Listener on Salary Range to display appropriate value.
+         - Validation of Name and Date
+*/
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary—output');
+    output.textContent = salary.value;
+    salary.addEventListener('input', function () {
+         output.textContent = salary.value;
+    });
+
+    const date = document.querySelector('#date');
+    date.addEventListener('input', function () 
+    {
+        let startDate = document.querySelector('#day').value + " " + document.querySelector('#month').value + " " +
+            document.querySelector('#year').value;
+        try 
+        {
+            (new EmployeePayroll()).startDate = new Date(Date.parse(startDate));
+            setTextValue('.date-error', "");
+        } 
+        catch (e) 
+        {
+            setTextValue('.date-error', e);
+        }
+    });
+});
+const setTextValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.textContent = value;
+}
+
+/* UC3:- Ability to create Employee Payroll Object On Save. 
+         - Validation of Name and Date and if failed then set the UI accordingly. */
+const save = () => {
+    try 
+    {
+        let EmployeePayRoll = createEmployeePayroll();
+    }
+    catch (e) 
+    {
+        alert(e);
+    }
+}
+
+const createEmployeePayroll = () => {
+    let employeePayrollData = new EmployeePayroll();
+    try {
+        employeePayrollData.name = getInputValueById('#name');
+    }
+    catch (e) {
+        setTextValue('.text-error', e);
+        throw e;
+    }
+
+    employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+    employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+    employeePayrollData.department = getSelectedValues('[name=department]');
+    employeePayrollData.salary = getInputValueById('#salary');
+    employeePayrollData.note = getInputValueById('#notes');
+    let date = getInputValueById('#day') + "," + getInputValueById('#month') + "," + getInputValueById('#year');
+    employeePayrollData.startDate = new Date(date);
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+}
+
+const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let sellItems = [];
+    allItems.forEach(item => {
+        if (item.checked)
+            sellItems.push(item.value);
+    });
+    return sellItems;
+}
+
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
+
+
+/* UC4:- Ability to save the Employee Payroll Object to Local Storage.
+ - Understand the difference between Local Storage, Session Storage and older feature of storing in cookies. Here are good references */
+function createAndUpdateStorage(employeePayrollData) 
+{
+    let employeePayrollList = [];
+    employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+    if (employeePayrollList != undefined) 
+    {
+        employeePayrollList.push(employeePayrollData);
+    }
+    else 
+    {
+        employeePayrollList = [employeePayrollData];
+    }
+    alert(employeePayrollList.toString());
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList))
+}
+
+
+
+/* UC5:- Ability to reset the form on clicking reset  */
+const resetForm = () => {
+    setValue('#name','');
+    unsetSelectedValues('[name=profile]');
+    unsetSelectedValues('[name=gender]');
+    unsetSelectedValues('[name=department]');
+    setValue('#salary', ' ');
+    setValue('#notes', ' ');
+    setValue('#day', '1');
+    setValue('#month', 'Jan');
+    setValue('#year', '2020');
+}
+const unsetSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach(item => { item.checked = false; }
+    );
+}
+const setValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.value = value;
+}
